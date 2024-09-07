b7bccba8a350f375196191b26207f5035063d26d
Jonnier <alzatejonny39@gmail.com>
2024-09-06 19:28:30 -0500

    xml casi finalizado

--- almacenamientoDeDatos/frmXml.Designer.cs
+++ almacenamientoDeDatos/frmXml.Designer.cs
@@ -28,19 +28,165 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtEdad = new System.Windows.Forms.TextBox();
+            this.lblId = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblEdad = new System.Windows.Forms.Label();
+            this.govInformacion = new System.Windows.Forms.GroupBox();
+            this.btnCrear = new System.Windows.Forms.Button();
+            this.btnLeer = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.govRegistros = new System.Windows.Forms.DataGridView();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.govInformacion.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.govRegistros)).BeginInit();
             this.SuspendLayout();
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(9, 56);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(100, 20);
+            this.txtId.TabIndex = 3;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(133, 56);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 4;
+            // 
+            // txtEdad
+            // 
+            this.txtEdad.Location = new System.Drawing.Point(262, 56);
+            this.txtEdad.Name = "txtEdad";
+            this.txtEdad.Size = new System.Drawing.Size(100, 20);
+            this.txtEdad.TabIndex = 5;
+            // 
+            // lblId
+            // 
+            this.lblId.AutoSize = true;
+            this.lblId.Location = new System.Drawing.Point(6, 37);
+            this.lblId.Name = "lblId";
+            this.lblId.Size = new System.Drawing.Size(16, 13);
+            this.lblId.TabIndex = 6;
+            this.lblId.Text = "Id";
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(130, 37);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(44, 13);
+            this.lblNombre.TabIndex = 7;
+            this.lblNombre.Text = "Nombre";
+            // 
+            // lblEdad
+            // 
+            this.lblEdad.AutoSize = true;
+            this.lblEdad.Location = new System.Drawing.Point(259, 37);
+            this.lblEdad.Name = "lblEdad";
+            this.lblEdad.Size = new System.Drawing.Size(32, 13);
+            this.lblEdad.TabIndex = 8;
+            this.lblEdad.Text = "Edad";
+            // 
+            // govInformacion
+            // 
+            this.govInformacion.Controls.Add(this.txtNombre);
+            this.govInformacion.Controls.Add(this.lblEdad);
+            this.govInformacion.Controls.Add(this.txtId);
+            this.govInformacion.Controls.Add(this.lblNombre);
+            this.govInformacion.Controls.Add(this.txtEdad);
+            this.govInformacion.Controls.Add(this.lblId);
+            this.govInformacion.Location = new System.Drawing.Point(92, 12);
+            this.govInformacion.Name = "govInformacion";
+            this.govInformacion.Size = new System.Drawing.Size(384, 87);
+            this.govInformacion.TabIndex = 9;
+            this.govInformacion.TabStop = false;
+            this.govInformacion.Text = "Info usuarios";
+            // 
+            // btnCrear
+            // 
+            this.btnCrear.Location = new System.Drawing.Point(92, 125);
+            this.btnCrear.Name = "btnCrear";
+            this.btnCrear.Size = new System.Drawing.Size(71, 32);
+            this.btnCrear.TabIndex = 10;
+            this.btnCrear.Text = "Crear";
+            this.btnCrear.UseVisualStyleBackColor = true;
+            this.btnCrear.Click += new System.EventHandler(this.btnCrear_Click);
+            // 
+            // btnLeer
+            // 
+            this.btnLeer.Location = new System.Drawing.Point(208, 124);
+            this.btnLeer.Name = "btnLeer";
+            this.btnLeer.Size = new System.Drawing.Size(71, 33);
+            this.btnLeer.TabIndex = 11;
+            this.btnLeer.Text = "Leer";
+            this.btnLeer.UseVisualStyleBackColor = true;
+            this.btnLeer.Click += new System.EventHandler(this.btnLeer_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(312, 124);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(71, 33);
+            this.btnEliminar.TabIndex = 12;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // govRegistros
+            // 
+            this.govRegistros.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.govRegistros.Location = new System.Drawing.Point(92, 181);
+            this.govRegistros.Name = "govRegistros";
+            this.govRegistros.Size = new System.Drawing.Size(384, 150);
+            this.govRegistros.TabIndex = 13;
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.Location = new System.Drawing.Point(405, 123);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(71, 32);
+            this.btnActualizar.TabIndex = 14;
+            this.btnActualizar.Text = "actualizar";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
             // 
             // frmXml
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(567, 450);
+            this.ClientSize = new System.Drawing.Size(567, 398);
+            this.Controls.Add(this.btnActualizar);
+            this.Controls.Add(this.govRegistros);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnLeer);
+            this.Controls.Add(this.btnCrear);
+            this.Controls.Add(this.govInformacion);
             this.Name = "frmXml";
             this.Text = "fmrXml";
+            this.govInformacion.ResumeLayout(false);
+            this.govInformacion.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.govRegistros)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtEdad;
+        private System.Windows.Forms.Label lblId;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblEdad;
+        private System.Windows.Forms.GroupBox govInformacion;
+        private System.Windows.Forms.Button btnCrear;
+        private System.Windows.Forms.Button btnLeer;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.DataGridView govRegistros;
+        private System.Windows.Forms.Button btnActualizar;
     }
 }
--- almacenamientoDeDatos/frmXml.cs
+++ almacenamientoDeDatos/frmXml.cs
@@ -1,20 +1,115 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Serialization;
+using System.IO;
+using System.Linq;
 
 namespace almacenamientoDeDatos
 {
     public partial class frmXml : Form
     {
+        string xmlArchivo = Environment.CurrentDirectory + "\\datosxml.xml"; 
+
         public frmXml()
         {
             InitializeComponent();
+        }
+
+        private void btnCrear_Click(object sender, EventArgs e)
+        {
+            List<person> p1 = new List<person>();
+            XmlSerializer serial = new XmlSerializer(typeof(List<person>));
+
+            
+            if (File.Exists(xmlArchivo))
+            {
+                using (FileStream fs = new FileStream(xmlArchivo, FileMode.Open, FileAccess.Read))
+                {
+                    p1 = serial.Deserialize(fs) as List<person>;
+                }
+            }
+
+            
+            int lnId = int.Parse(txtId.Text);
+            string lcNombre = txtNombre.Text;
+            int lnEdad = int.Parse(txtEdad.Text);
+            p1.Add(new person() { id = lnId, nombre = lcNombre, edad = lnEdad });
+
+            
+            using (FileStream fs = new FileStream(xmlArchivo, FileMode.Create, FileAccess.Write))
+            {
+                serial.Serialize(fs, p1);
+                MessageBox.Show("Registro agregado");
+            }
+
+        }
+
+        private void btnLeer_Click(object sender, EventArgs e)
+        {
+            List<person> p1 = new List<person>();
+            XmlSerializer serial = new XmlSerializer(typeof(List<person>));
+
+            
+            if (File.Exists(xmlArchivo))
+            {
+                using (FileStream fs = new FileStream(xmlArchivo, FileMode.Open, FileAccess.Read))
+                {
+                    p1 = serial.Deserialize(fs) as List<person>;
+                }
+
+                
+                govRegistros.DataSource = p1;
+            }
+            else
+            {
+                MessageBox.Show("El archivo XML no existe.");
+            }
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            List<person> p1 = new List<person>();
+            XmlSerializer serial = new XmlSerializer(typeof(List<person>));
+
+            
+            if (File.Exists(xmlArchivo))
+            {
+                
+                using (FileStream fs = new FileStream(xmlArchivo, FileMode.Open, FileAccess.Read))
+                {
+                    p1 = serial.Deserialize(fs) as List<person>;
+                }
+
+                
+                int lnId = int.Parse(txtId.Text);
+
+                var lcPersonaEliminar = p1.FirstOrDefault(p => p.id == lnId);
+
+                if (lcPersonaEliminar != null)
+                {
+                    p1.Remove(lcPersonaEliminar);
+                    using (FileStream fs = new FileStream(xmlArchivo, FileMode.Create, FileAccess.Write))
+                    {
+                        serial.Serialize(fs, p1);
+                    }
+
+                    MessageBox.Show("Registro eliminado.");
+                }
+                else
+                {
+                    MessageBox.Show("No se encontró un registro con ese ID.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("El archivo XML no existe.");
+            }
+
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
         }
     }
 }
